9156422c6ea0640c1689e4431e2248edfd0c2a7c
jinglige <leejing16306@163.com>
2023-09-17 22:33:07 +0800

    Binary Tree

---
+++ src/main/java/com/lee/datastructure/binarytree/Leetcode102.java
@@ -0,0 +1,83 @@
+package com.lee.datastructure.binarytree;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 102. 二叉树的层序遍历
+ * 给你二叉树的根节点 root ，返回其节点值的层序遍历。（即逐层地，从左到右访问所有节点）。
+ *
+ * @author LiJing
+ * @version 1.0
+ */
+public class Leetcode102 {
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        LinkedList<TreeNode> queue = new LinkedList<>(); // 队列
+        queue.offer(root);
+        int c1 = 1; // 当前层节点数
+        while (!queue.isEmpty()) {
+            int c2 = 0; // 统计下一层节点数
+            List<Integer> level = new ArrayList<>(); // 保存当前层结果
+            for (int i = 0; i < c1; i++) {
+                TreeNode node = queue.poll();
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                    c2++;
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                    c2++;
+                }
+            }
+            result.add(level);
+            c1 = c2;
+        }
+        return result;
+    }
+
+    /*
+                  1
+                 / \
+                2   3
+               /\   /\
+              4  5 6  7
+
+        队列：  头 [] 尾
+
+        [
+            [1]
+            [2,3]
+            [4,5,6,7]
+        ]
+     */
+
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(
+                new TreeNode(
+                        new TreeNode(4),
+                        2,
+                        new TreeNode(5)
+                ),
+                1,
+                new TreeNode(
+                        new TreeNode(6),
+                        3,
+                        new TreeNode(7)
+                )
+        );
+
+        List<List<Integer>> result = new Leetcode102().levelOrder(root);
+        for (List<Integer> list : result) {
+            System.out.println(list);
+        }
+    }
+}
+
+
---
+++ src/main/java/com/lee/datastructure/binarytree/TreeNode.java
@@ -0,0 +1,28 @@
+package com.lee.datastructure.binarytree;
+
+/**
+ * 树节点类
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+    public TreeNode() {
+    }
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+
+    public TreeNode(TreeNode left, int val, TreeNode right) {
+        this.left = left;
+        this.val = val;
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return String.valueOf(this.val);
+    }
+}
